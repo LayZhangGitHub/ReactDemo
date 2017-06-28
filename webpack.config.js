4e3612515e42140617dfc451a53bbbdb7417d023
zhanglei <zhanglei@zhixunkeji.cn>
2017-06-28 19:46:14 +0800

    add react demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
-var webpack = require("webpack");
-var path = require("path");
 
-var DEV = path.resolve(__dirname, "Dev");
-var OUTPUT = path.resolve(__dirname, "output");
 
-var config = {
-    entry: DEV + "/index.jsx",
-    output: {
-        path: OUTPUT,
-        filename: "myCode.js"
+var webpack = require('webpack');
+// var commonsPlugin = new webpack.optimize.CommonsChunkPlugin('common.js');
+var path = require('path');
+
+module.exports = {
+
+    //页面入口文件配置
+    entry: {
+        index: './src/index.jsx'
     },
+    //入口文件输出配置
+    output: {
+
+        path: path.join(__dirname, "/assets"),
+        filename: "bundle.js",
+        // publicPath: "/assets/" //发布时 指定资源引用路径
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     module: {
         loaders: [{
-            include: DEV,
-            loader: "babel",
+            include: path.join(__dirname, "/src"),
+            loader: "babel-loader",
         }]
     }
 };
-
-module.exports = config;
